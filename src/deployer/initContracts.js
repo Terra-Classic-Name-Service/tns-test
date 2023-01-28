83b332ed0dc897259118f232b0df431109d9370b
syslink <7135202@qq.com>
2023-01-28 14:38:15 +0800

    add deployer and init

---
+++ src/deployer/initContracts.js
@@ -0,0 +1,125 @@
+import { Coins, LCDClient, MsgInstantiateContract, MnemonicKey, isTxError } from '@terra-money/terra.js';
+import * as fs from 'fs';
+import fetch from 'isomorphic-fetch';
+import pkg from 'js-sha3';
+import { namehash } from '../utils.js';
+
+const { sha3_256: SHA256 } = pkg;
+
+//const gasPrices =  await fetch('https://columbus-fcd.terra.dev/v1/txs/gas_prices');
+const gasPricesJson = {"uluna":"28.325","usdr":"0.52469","uusd":"0.75","ukrw":"850.0","umnt":"2142.855","ueur":"0.625","ucny":"4.9","ujpy":"81.85","ugbp":"0.55","uinr":"54.4","ucad":"0.95","uchf":"0.7","uaud":"0.95","usgd":"1.0","uthb":"23.1","usek":"6.25","unok":"6.25","udkk":"4.5","uidr":"10900.0","uphp":"38.0","uhkd":"5.85","umyr":"3.0","utwd":"20.0"};
+const gasPricesCoins = new Coins(gasPricesJson); 
+
+console.log(gasPricesCoins)
+// test1 key from localterra accounts
+const mk = new MnemonicKey({
+  mnemonic: ''
+})
+
+// connect to localterra
+const terra = new LCDClient({
+  URL: 'https://lcd.terra.dev/',
+  chainID: 'columbus-5',
+  gasPrices: gasPricesCoins,
+  gasAdjustment: "1.5", // Increase gas price slightly so transactions go through smoothly.
+  gas: 10000000,
+});
+
+const wallet = terra.wallet(mk);
+
+const contractNames = ['registry', 'resolver', 'registrar', 'controller'];
+
+const contractAddrs = {'registry': 'terra18nsn4nm32lf3hsky6l582xg5g92kqt829ckfsx', 
+                       'resolver': 'terra178zzd6lyp7ucykjjep3zmlv8kun8xn7t08fxcr',
+                       'registrar': 'terra1w5a62a4jgqq8a08hsmphn6ad7caw5mqamh8hq0',
+                       'controller': 'terra1agx8ruthzfe6vpw3tu43kc32396dge67rc5tap'}
+
+const contractCodeId = {
+    'controller': [6792],
+    'registrar': [6793],
+    'resolver': [6794],
+    'registry': [6796]
+}
+
+const initInfo = {
+    'controller': {
+        registrar_address: '',
+        max_commitment_age: 3600,
+        min_commitment_age: 30,
+        min_registration_duration: 90 * 3600 * 24,
+        tier1_price: 1000000,
+        tier2_price: 500000,
+        tier3_price: 250000,
+        enable_registration: true,
+        enable_public_mint: false,
+        start_time: 1673168358,
+        end_time: 1674032358,
+        phase: 1        
+    },
+    'registrar': {
+        registry_address: '',
+        base_node: namehash('lunc').substr(2),
+        base_name: 'lunc',
+        reverse_node: namehash('reverse').substr(2),
+        reverse_name: 'reverse',
+        grace_period: 30 * 24 * 3600,
+        name: 'Terra Name Service',
+        symbol: 'TNS'
+    },
+    'registry': {
+    },
+    'resolver': {
+      registry_address: '',
+      interface_id: 1
+    }
+}
+
+for (let i =0; i < contractNames.length; i++) {
+  const contractName = contractNames[i];
+  if (contractAddrs[contractName] != null && contractAddrs[contractName] != '') continue;
+
+  console.log('start init ', contractName);
+  const codeId = contractCodeId[contractName];
+  const initMsg = initInfo[contractName];
+  if (contractName == 'controller') {
+    initMsg.registrar_address = contractAddrs['registrar'];
+  } else if (contractName == 'registrar') {
+    initMsg.registry_address = contractAddrs['registry'];
+  } else if (contractName == 'resolver') {
+    initMsg.registry_address = contractAddrs['registry'];
+  } 
+  console.log(initMsg);
+  await initContract(contractName, codeId, initMsg);
+}
+
+async function initContract(contractName, codeId, initMsg) {
+  const instantiate = new MsgInstantiateContract(
+    wallet.key.accAddress,
+    '',
+    +codeId, // code ID
+    initMsg, // InitMsg
+    {}, // init coins
+    false // migratable
+  );
+
+  const instantiateTx = await wallet.createAndSignTx({
+    msgs: [instantiate],
+    feeDenoms: ['uluna']
+  });
+  const instantiateTxResult = await terra.tx.broadcast(instantiateTx);
+
+  console.log(contractName, instantiateTxResult.txhash);
+
+  if (isTxError(instantiateTxResult)) {
+    throw new Error(
+      `store code failed. code: ${instantiateTxResult.code}, codespace: ${instantiateTxResult.codespace}, raw_log: ${instantiateTxResult.raw_log}`
+    );
+  }
+
+  const {
+      instantiate_contract: { contract_address },
+  } = instantiateTxResult.logs[0].eventsByType;
+
+  contractAddrs[contractName] = contract_address;
+  console.log(contractName, contract_address);
+}
---
+++ src/deployer/storeContracts.js
@@ -0,0 +1,62 @@
+import { Coins, LCDClient, MsgStoreCode, MnemonicKey, isTxError } from '@terra-money/terra.js';
+import * as fs from 'fs';
+import fetch from 'isomorphic-fetch';
+
+// const gasPrices =  await fetch('https://columbus-fcd.terra.dev/v1/txs/gas_prices');
+// const gasPricesJson = await gasPrices.json();
+const gasPricesJson = {"uluna":"28.325","usdr":"0.104938","uusd":"0.15","ukrw":"170.0","umnt":"428.571","ueur":"0.125","ucny":"0.98","ujpy":"16.37","ugbp":"0.11","uinr":"10.88","ucad":"0.19","uchf":"0.14","uaud":"0.19","usgd":"0.2","uthb":"4.62","usek":"1.25","unok":"1.25","udkk":"0.9","uidr":"2180.0","uphp":"7.6","uhkd":"1.17","umyr":"0.6","utwd":"4.0"};
+const gasPricesCoins = new Coins(gasPricesJson); 
+
+const mk = new MnemonicKey({
+  mnemonic: ''
+})
+
+// connect to localterra
+const terra = new LCDClient({
+  URL: 'https://lcd.terra.dev/',
+  chainID: 'columbus-5',
+  gasPrices: gasPricesCoins,
+  gasAdjustment: "1.5", // Increase gas price slightly so transactions go through smoothly.
+  gas: 10000000,
+});
+
+const wallet = terra.wallet(mk);
+console.log(wallet.key.accAddress);
+
+async function storeCode2TerraClassic(filePath) {
+  const storeCode = new MsgStoreCode(
+    wallet.key.accAddress,
+    fs.readFileSync(filePath).toString('base64')
+  );
+  const storeCodeTx = await wallet.createAndSignTx({
+    msgs: [storeCode],
+    feeDenoms: ['uluna']
+  });
+  const storeCodeTxResult = await terra.tx.broadcast(storeCodeTx);
+  
+  if (isTxError(storeCodeTxResult)) {
+    throw new Error(
+      `store code failed. code: ${storeCodeTxResult.code}, codespace: ${storeCodeTxResult.codespace}, raw_log: ${storeCodeTxResult.raw_log}`
+    );
+  }
+  
+  const {
+    store_code: { code_id },
+  } = storeCodeTxResult.logs[0].eventsByType;
+
+  console.log(filePath, '=>', code_id);
+}
+
+const path = "/Users/mryu/blockchain/terra-classic/dapp/tns/artifacts/";
+//const contractNames = ["controller.wasm", "registrar.wasm", "registry.wasm", "resolver.wasm"];
+
+for (let i = 2; i < process.argv.length; i++) {
+  const fileName = process.argv[i];
+  const filePath = path + fileName;
+  await storeCode2TerraClassic(filePath);
+}
+
+// controller.wasm => [ '6792' ]
+// registrar.wasm => [ '6793' ]
+// resolver.wasm => [ '6794' ]
+// registry.wasm => [ '6795' ]
