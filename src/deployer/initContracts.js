87152f231f7f28aa50552bc8dfdf9c9cad37d11c
syslink <7135202@qq.com>
2023-02-23 19:15:26 +0800

    deploy new contract

--- src/deployer/initContracts.js
+++ src/deployer/initContracts.js
@@ -1,34 +1,34 @@
 import { Coins, LCDClient, MsgInstantiateContract, MnemonicKey, isTxError } from '@terra-money/terra.js';
-// import { toUnicode } from 'idna-uts46-hx';
-import pkg from 'js-sha3';
-import pkgIdna from 'idna-uts46-hx';
+import { namehash } from '../utils.js';
+// import pkg from 'js-sha3';
+// import pkgIdna from 'idna-uts46-hx';
 
-const { sha3_256: SHA256 } = pkg;
-const { toUnicode } = pkgIdna;
+// const { sha3_256: SHA256 } = pkg;
+// const { toUnicode } = pkgIdna;
 
-const namehash = (inputName) => {
-  var node = ''
-  for (var i = 0; i < 32; i++) {
-    node += '00'
-  }
+// const namehash = (inputName) => {
+//   var node = ''
+//   for (var i = 0; i < 32; i++) {
+//     node += '00'
+//   }
 
-  var name = normalize(inputName)
+//   var name = normalize(inputName)
 
-  if (name) {
-    var labels = name.split('.')
+//   if (name) {
+//     var labels = name.split('.')
 
-    for(var i = labels.length - 1; i >= 0; i--) {
-      var labelSha = SHA256(labels[i])
-      node = SHA256(new Buffer(node + labelSha, 'hex'))
-    }
-  }
+//     for(var i = labels.length - 1; i >= 0; i--) {
+//       var labelSha = SHA256(labels[i])
+//       node = SHA256(new Buffer(node + labelSha, 'hex'))
+//     }
+//   }
 
-  return '0x' + node
-}
+//   return '0x' + node
+// }
 
-function normalize(name) {
-  return name ? toUnicode(name, {useStd3ASCII: true, transitional: false}) : name
-}
+// function normalize(name) {
+//   return name ? toUnicode(name, {useStd3ASCII: true, transitional: false}) : name
+// }
 
 //const gasPrices =  await fetch('https://columbus-fcd.terra.dev/v1/txs/gas_prices');
 const gasPricesJson = {"uluna":"28.325","usdr":"0.52469","uusd":"0.75","ukrw":"850.0","umnt":"2142.855","ueur":"0.625","ucny":"4.9","ujpy":"81.85","ugbp":"0.55","uinr":"54.4","ucad":"0.95","uchf":"0.7","uaud":"0.95","usgd":"1.0","uthb":"23.1","usek":"6.25","unok":"6.25","udkk":"4.5","uidr":"10900.0","uphp":"38.0","uhkd":"5.85","umyr":"3.0","utwd":"20.0"};
@@ -63,10 +63,10 @@ const wallet = terra.wallet(mk);
 
 const contractNames = ['registry', 'resolver', 'registrar', 'controller'];
 
-const contractAddrs = {'registry': 'terra18nsn4nm32lf3hsky6l582xg5g92kqt829ckfsx', 
-                       'resolver': 'terra178zzd6lyp7ucykjjep3zmlv8kun8xn7t08fxcr',
-                       'registrar': 'terra1fswjjgx2ql0l3cg58uhjhaanqqv8cfqfrc9mqf',
-                       'controller': 'terra1zhcw620ma2vlpwvsr27hjf53ng8lgdm8egkg98'}
+const contractAddrs = {'registry': 'terra12jjtksj6lwpsf8fjffx4gcw45ypl9s0ud92eff', 
+                       'resolver': 'terra1pnxm63djh6k0mqvnw22uenv55tg7zjd2d4mfa3',
+                       'registrar': 'terra1er34wfk0hjpawtpfqp2kwfqaj9tm0aefl8uwce',
+                       'controller': 'terra1sum7a9qy43lkxk29yv8tjnm2uevp6nwgcxdw62'}
 
 const contractCodeId = {
     'controller': [6813],
@@ -103,7 +103,7 @@ const initInfo = {
         enable_registration: true,
         enable_public_mint: false,
         start_time: 1673168358,
-        end_time: 1674032358,
+        end_time: 1684032358,
         phase: 1        
     },
     'registrar': {
